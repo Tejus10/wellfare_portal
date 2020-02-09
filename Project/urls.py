81458b0da71a3e23ebf79ee191bfdf79bbdc36a7
Shashwat Sharma <shashwat.sharma@iitg.ac.in>
2020-02-09 12:23:06 +0530

    removed unused accounts pattern from urls.py

--- Project/urls.py
+++ Project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('/',include('feed.urls'))
-    path('accounts/',include('django.contrib.auth.urls')),
+    # path('accounts/',include('django.contrib.auth.urls')),
     path('home/',include('feed.urls'))
 ]
